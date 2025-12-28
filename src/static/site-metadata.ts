db29c9d5f0799eb219bed797087b988ee4f1d178
chenguixu <guixuchen2233@foxmail.com>
2025-12-28 19:34:25 +0800

    访问方式调整

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://running.chenxu2233.com',
+  siteUrl: 'https://chenguixu2233.github.io/running_page',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
